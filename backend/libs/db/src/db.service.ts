a423f2f5406a99d27c86844cfce1d0ee180a95c0
Roman Sirokov <roman@mrlightful.com>
2025-07-20 11:46:34 +0200

    Simplify drizzle setup

--- backend/libs/db/src/db.service.ts
+++ backend/libs/db/src/db.service.ts
@@ -1,17 +1,13 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { DatabaseConfig } from './db.config';
-import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 
 export type Database = ReturnType<typeof drizzle>;
 
 @Injectable()
 export class DatabaseService {
-  private readonly logger = new Logger(DatabaseService.name);
   readonly db: Database;
   constructor(databaseConfig: DatabaseConfig) {
-    const client = createClient({ url: databaseConfig.fileName });
-    this.db = drizzle(client);
-    this.logger.log(`Database connection initialized`);
+    this.db = drizzle(databaseConfig.fileName);
   }
 }
